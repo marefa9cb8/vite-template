91a6b9a182867655e0bd998207e0508f7fc61225
marefa9cb8 <marefa9cb8@gmail.com>
2024-04-07 13:51:18 +0900

    vite+tailwind

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  settings: { react: { version: 'detect' } },
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  quoteProps: 'preserve',
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+}
