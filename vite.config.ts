00ff364db036efa65b5cc8e1cde866259f57c6b6
marefa9cb8 <marefa9cb8@gmail.com>
2025-03-15 12:10:16 +0900

    tailwind4に合わせて設定変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: { alias: { '@': path.resolve(__dirname, './src') } },
   test: { globals: true },
 }))
