8f29d82ae429ade6cfb07571c41b294d6e198a44
marefa9cb8 <marefa9cb8@gmail.com>
2025-03-24 22:52:30 +0900

    recommendedを追加

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import eslintPluginReact from 'eslint-plugin-react'
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
@@ -17,13 +18,23 @@ export default [
       globals: { ...globals.browser },
       parserOptions: { project: './tsconfig.json' },
     },
-    plugins: { react: eslintPluginReact },
     linterOptions: { reportUnusedDisableDirectives: 'error' },
+    settings: { react: { version: 'detect' } },
   },
   js.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
+  eslintPluginReact.configs.flat.recommended,
+  eslintPluginReact.configs.flat['jsx-runtime'],
+  eslintPluginReactHooks.configs['recommended-latest'],
   eslintConfigPrettier,
+  {
+    name: 'Type Importを強制するRules',
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-import-type-side-effects': 'error',
+    },
+  },
   {
     name: '一時的にOFFにしたRules',
     rules: {},
