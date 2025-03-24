1d3022a1ed1d18557cdda299f44e4ce44baa8f01
balar <marefa9cb8@gmail.com>
2025-03-24 22:53:39 +0900

    パッケージの更新と設定ファイルの強化 (#72)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  prefix: '',
-}
+} as const as Config
